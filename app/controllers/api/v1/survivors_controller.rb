e5aa692e911fee76e341ed253c68cbc0e8c8ebeb
Matheus Azevedo <azevedooliveiramatheus@gmail.com>
2020-08-30 11:14:48 -0300

    refining seeds and adding more readable output from survivors

--- app/controllers/api/v1/survivors_controller.rb
+++ app/controllers/api/v1/survivors_controller.rb
@@ -3,9 +3,8 @@ class Api::V1::SurvivorsController < ApplicationController
     # GET /api/survivors --> Get all the survivors, non-infected included;
     def index
         puts "getting all survivors"
-        @survivors = Survivor.all
-
-        render json: {"total" => @survivors.count, "survivors" => @survivors}
+        survivors = Survivor.all
+        render json: {"total" => survivors.count, "survivors" => survivors.select(:id, :name, :age, :gender, :reports, :infected) } , status: :ok
         # render json: @survivors
     end
 
@@ -13,57 +12,60 @@ class Api::V1::SurvivorsController < ApplicationController
     def show 
         puts "get one single survivor"
         params.require(:id)
-        @survivor = Survivor.find(params.permit(:id)[:id])
-        render json: {survivor: @survivor, last_location: @survivor.location, inventory: [ @survivor.inventory, items: @survivor.inventory.items ] }
+        survivor = Survivor.select(:id, :name, :age, :gender, :reports, :infected).where(id: params.permit(:id)[:id]).first
+        location = Location.select(:id, :latitude, :longitude).where(id: survivor.id).first
+        inventory = Inventory.select(:id, :capacity).where(survivor_id: survivor.id).first
+        items = Item.select(:id, :name, :value, :weight, :amount).where(inventory_id: inventory.id)
+        render json: {survivor: survivor, last_location: location, inventory: [ inventory, items: items ] }, status: :ok 
     end
     
     # POST /api/survivor --> Adds a new survivor to the database;
     def create
-        @survivor = Survivor.new(survivor_params)
-        @survivor.location = Location.new(location_params) 
-        @survivor.inventory = Inventory.new() 
-        @survivor.inventory.items.push(Item.new(:name=> "water", :value => 4, :weight => 2))
-        @survivor.inventory.items.push(Item.new(:name => "food", :value => 3 , :weight => 3))
-        @survivor.inventory.items.push(Item.new(:name => "medication", :value => 2 , :weight => 1))
-        @survivor.inventory.items.push(Item.new(:name => "ammunition", :value => 1 , :weight => 4))
-        @survivor.inventory.items.push(Item.new(:name => "coin", :value => 1 , :weight => 0))
-        # @survivor.inventory.save
-        @items = JSON.parse(params.require(:items))
+        survivor = Survivor.new(survivor_params)
+        survivor.location = Location.new(location_params) 
+        survivor.inventory = Inventory.new() 
+        survivor.inventory.items.push(Item.new(:name=> "water", :value => 4, :weight => 2))
+        survivor.inventory.items.push(Item.new(:name => "food", :value => 3 , :weight => 3))
+        survivor.inventory.items.push(Item.new(:name => "medication", :value => 2 , :weight => 1))
+        survivor.inventory.items.push(Item.new(:name => "ammunition", :value => 1 , :weight => 4))
+        survivor.inventory.items.push(Item.new(:name => "coin", :value => 1 , :weight => 0))
+        # survivor.inventory.save
+        items = JSON.parse(params.require(:items))
         keys = {"water" => 0, "food" => 1, "medication" => 2, "ammunition" => 3, "coin" => 4}
         keys.default = 5
         puts keys["water"]
         puts keys["asd"]
-        @capacity = 0
-        @items.each do |item|
-            @item = @survivor.inventory.items[keys[item.first]]
+        capacity = 0
+        items.each do |item|
+            sur_good = survivor.inventory.items[keys[item.first]]
             puts "item"
-            puts @item
-            if !@item.nil?
-                @item.amount += item.last
-                @capacity += @item.weight * item.last
+            puts item
+            if !sur_good.nil?
+                sur_good.amount += item.last
+                capacity += sur_good.weight * item.last
             end
         end
-       @survivor.inventory.capacity = params[:capacity].nil? ? @capacity * 2 : params[:capacity]
-       if @survivor.save
-          head :no_content
+       survivor.inventory.capacity = params[:capacity].nil? ? capacity * 2 : params[:capacity]
+       if survivor.save
+          render json: survivor, status: :created
        else
-          render json: @survivor.errors, status: :unprocessable_entity
+          render json: survivor.errors, status: :unprocessable_entity
        end
     end
    
     # PUT /api/survivor --> Updates the location of one survivor;
     def update_location
-        @survivor = params.require(:survivor_id).permit(:survivor_id)
-        if !@survivor.nil?
+        survivor = params.require(:survivor_id).permit(:survivor_id)
+        if !survivor.nil?
             puts "updating survivor location"
-            @survivor.location = Location.new(location_params)
-            if @survivor.save
+            survivor.location = Location.new(location_params)
+            if survivor.save
                 head :no_content
             else
-                render json: @survivor.errors, status: :unprocessable_entity
+                render json: survivor.errors, status: :unprocessable_entity
             end
         else
-            render json: @survivor.errors, status: :unprocessable_entity
+            render json: survivor.errors, status: :unprocessable_entity
         end
     end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ puts "seeding..."
 
 s = Survivor.new(name: "Jorge", age: "21", gender: "Male")
 s.location = Location.new(latitude: 318273, longitude: 6532)
-s.inventory = Inventory.new()
+s.inventory = Inventory.new(capacity: 148)
 s.inventory.items.push(Item.new(name: "water",      value: 4, weight: 2, amount: 3))
 s.inventory.items.push(Item.new(name: "food",       value: 3, weight: 3, amount: 4))
 s.inventory.items.push(Item.new(name: "medication", value: 2, weight: 1, amount: 8))
@@ -12,7 +12,7 @@ s.save
 
 s = Survivor.new(name: "Marge", age: "32", gender: "Female")
 s.location = Location.new(latitude: 452, longitude: 5452)
-s.inventory = Inventory.new()
+s.inventory = Inventory.new(capacity: 172)
 s.inventory.items.push(Item.new(name: "water",      value: 4, weight: 2, amount: 8))
 s.inventory.items.push(Item.new(name: "food",       value: 3, weight: 3, amount: 3))
 s.inventory.items.push(Item.new(name: "medication", value: 2, weight: 1, amount: 5))
@@ -20,9 +20,9 @@ s.inventory.items.push(Item.new(name: "ammunition", value: 1, weight: 4, amount:
 s.inventory.items.push(Item.new(name: "coin",       value: 1, weight: 0, amount: 521))
 s.save 
 
-s = Survivor.new(name: "Phill", age: "19", gender: "Male")
+s = Survivor.new(name: "Phill", age: "19", gender: "Male", reports: 2)
 s.location = Location.new(latitude: 741823, longitude: 4521)
-s.inventory = Inventory.new()
+s.inventory = Inventory.new(capacity: 32)
 s.inventory.items.push(Item.new(name: "water",      value: 4, weight: 2, amount: 1))
 s.inventory.items.push(Item.new(name: "food",       value: 3, weight: 3, amount: 2))
 s.inventory.items.push(Item.new(name: "medication", value: 2, weight: 1, amount: 0))
@@ -30,9 +30,9 @@ s.inventory.items.push(Item.new(name: "ammunition", value: 1, weight: 4, amount:
 s.inventory.items.push(Item.new(name: "coin",       value: 1, weight: 0, amount: 0))
 s.save 
 
-s = Survivor.new(name: "Pamn", age: "24", gender: "Female")
+s = Survivor.new(name: "Pamn", age: "24", gender: "Female", reports: 1)
 s.location = Location.new(latitude: 86357, longitude: 153542)
-s.inventory = Inventory.new()
+s.inventory = Inventory.new(capacity: 820)
 s.inventory.items.push(Item.new(name: "water",      value: 4, weight: 2, amount: 18))
 s.inventory.items.push(Item.new(name: "food",       value: 3, weight: 3, amount: 33))
 s.inventory.items.push(Item.new(name: "medication", value: 2, weight: 1, amount: 15))
@@ -42,7 +42,7 @@ s.save
 
 s = Survivor.new(name: "John", age: "67", gender: "Male")
 s.location = Location.new(latitude: 452, longitude: 5452)
-s.inventory = Inventory.new()
+s.inventory = Inventory.new(capacity: 3150)
 s.inventory.items.push(Item.new(name: "water",      value: 4, weight: 2, amount: 3))
 s.inventory.items.push(Item.new(name: "food",       value: 3, weight: 3, amount: 5))
 s.inventory.items.push(Item.new(name: "medication", value: 2, weight: 1, amount: 6))
@@ -52,7 +52,7 @@ s.save
 
 s = Survivor.new(name: "Matthew", age: "31", gender: "Male", infected: true, reports: 6)
 s.location = Location.new(latitude: 12, longitude: 421)
-s.inventory = Inventory.new()
+s.inventory = Inventory.new(capacity: 56)
 s.inventory.items.push(Item.new(name: "water",      value: 4, weight: 2, amount: 5))
 s.inventory.items.push(Item.new(name: "food",       value: 3, weight: 3, amount: 2))
 s.inventory.items.push(Item.new(name: "medication", value: 2, weight: 1, amount: 12))
@@ -62,7 +62,7 @@ s.save
 
 s = Survivor.new(name: "Kayle", age: "42", gender: "Female", infected: true, reports: 4)
 s.location = Location.new(latitude: 6881, longitude: 42149)
-s.inventory = Inventory.new()
+s.inventory = Inventory.new(capacity: 158)
 s.inventory.items.push(Item.new(name: "water",      value: 4, weight: 2, amount: 0))
 s.inventory.items.push(Item.new(name: "food",       value: 3, weight: 3, amount: 2))
 s.inventory.items.push(Item.new(name: "medication", value: 2, weight: 1, amount: 33))
